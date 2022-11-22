cbbdcd728a8963761d8fb31fd29bd3f5f08e0f58
NadiaMonzon <nadiamonque99@gmail.com>
2022-11-22 18:47:38 +0000

    feat: add fetch

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 "use strict";
+const quote = document.querySelector(".js-quote");
+const author = document.querySelector(".js-author");
+const quoteButton = document.querySelector(".js-quote-button");
+
+fetch("http://api.quotable.io/random")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    quote.innerHTML = data.content;
+    quote.id = data._id;
+    author.innerHTML = data.author;
+  });
